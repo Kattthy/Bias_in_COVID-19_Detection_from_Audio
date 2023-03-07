c25810ff875f239ea658031c8ca2df1b62b2c77f
Kattthy <wujiaqi@usc.edu>
2023-03-07 14:48:15 -0800

    Add AudioProcessing (convert2wav & wav2mfcc).

---
+++ AudioProcessing.py
@@ -0,0 +1,61 @@
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import numpy as np
+import pandas as pd
+from os import listdir
+from os.path import isfile, join
+from pydub import AudioSegment
+import sys
+from tqdm import tqdm
+
+class WavConverter:
+    def __init__(self, root_path, file_name):
+        self.root_path = root_path # e.g. './coughvid_20211012/'
+        self.file_name = file_name # e.g. 'XXXX.ogg'
+        self.input_file = root_path+file_name # e.g. './coughvid_20211012/XXXX.ogg'
+
+    def convert_to_wav(self):
+        # Audio conversion code goes here
+        # This method converts the input file to .wav format
+        uuid, file_type = self.file_name.split('.')
+        if file_type == 'ogg':
+            ogg_audio = AudioSegment.from_ogg(self.input_file)
+            ogg_audio.export(self.root_path + uuid + ".wav", format="wav")
+        else: # webm
+            webm_audio = AudioSegment.from_file(self.input_file, format="webm")
+            webm_audio.export(self.root_path + uuid + ".wav", format="wav")
+
+if __name__ == '__main__':
+    metadata = pd.read_csv('./coughvid_20211012/metadata_compiled.csv')
+    metadata = metadata[["uuid", "cough_detected","age","gender","status"]]
+    metadata = metadata.dropna()
+    metadata = metadata[(metadata['cough_detected']>=0.9)]
+    uuid_set = set(metadata['uuid'])
+    audiofiles = [f for f in listdir('./coughvid_20211012/') if isfile(join('./coughvid_20211012/', f))]
+    root_path = './coughvid_20211012/'
+    
+    if sys.argv[1] == 'convert2wav':
+        for f in tqdm(audiofiles): # f: XXXX.ogg
+            uuid, file_type = f.split('.')
+            if uuid not in uuid_set:
+                continue
+            if file_type == 'ogg' or file_type == 'webm':
+                WavConverter(root_path, f).convert_to_wav()
+    
+    elif sys.argv[1] == 'wav2mfcc':
+        mfcc_root_path = root_path + 'mfcc/'
+        for uuid in tqdm(list(uuid_set)):
+            wav_path = root_path + uuid + '.wav'
+            y, sr = librosa.load(wav_path)
+            S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
+            log_S = librosa.power_to_db(S, ref=np.max)
+            mfcc = librosa.feature.mfcc(S=log_S, n_mfcc=13)
+            fig = plt.Figure()
+            canvas = FigureCanvas(fig)
+            ax = fig.add_subplot(111)
+            p = librosa.display.specshow(mfcc, ax=ax)
+            fig_path = mfcc_root_path + uuid + '.png'
+            fig.savefig(fig_path)
+    
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Bias_in_COVID-19_Detection_from_Audio
 This project will focus on looking for biases that exist in the COVID-19 detection based on coughing sounds.
+
+
+
+* DataPreprocessing.ipynb
+
+  * Filter the data with cough_detected below 0.9;
+  * Divided the age into 4 groups: <=20, 20-40, 40-60, >60;
+  * Balanced dataset across the three status classes (COVID-19, Healthy, and Symptomatic) by randomly picking 519 samples from each class;
+  * Split the dataset to traning set, validation set, and test set
+  * Analyze the dataset by gender and age
+
+* dataset_split
+
+  Saving files of spliting dataset. Could apply different dataset split methods here.
+
+  * metadata_balanced_test.csv: 
+  * metadata_balanced_train.csv
+  * metadata_balanced_val.csv
+
+* coughvid_20211012
+
+  * .wav files: audios
+  * /mfcc: mfcc images
+
+* AudioProcessing.py
+
+  It is for processing the audio. Now it consists of two methods. Run it by `python AudioProcessing.py [task]`.
+
+  [task] can convert2wav / wav2mfcc.
+
+  * convert2wav: convert all the ogg / webm audios files to wav files.
+  * wav2mfcc: generate mfcc images from wav files, and save them to ./coughvid_20211012/mfcc/.
