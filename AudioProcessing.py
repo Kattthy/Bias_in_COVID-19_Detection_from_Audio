64742c7870614d523ab9d78c9a5aaeacb1d3b887
Kattthy <wujiaqi@usc.edu>
2023-03-13 23:16:12 -0700

    Downsample the audios and split the audios to audio chunks (2s each).

--- AudioProcessing.py
+++ AudioProcessing.py
@@ -43,33 +43,57 @@ if __name__ == '__main__':
                 continue
             if file_type == 'ogg' or file_type == 'webm':
                 WavConverter(root_path, f).convert_to_wav()
-    
+
     elif sys.argv[1] == 'wav2mfcc':
-        mfcc_root_path = root_path + 'mfcc/'
+        mfcc_root_path = root_path + 'mfcc_chunk/'
+        total_count = 0
         for uuid in tqdm(list(uuid_set)):
+            # wav_path = root_path + uuid + '.wav'
+            # y, sr = librosa.load(wav_path)
+            # S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
+            # log_S = librosa.power_to_db(S, ref=np.max)
+            # mfcc = librosa.feature.mfcc(S=log_S, n_mfcc=13)
+            # fig = plt.Figure()
+            # canvas = FigureCanvas(fig)
+            # ax = fig.add_subplot(111)
+            # p = librosa.display.specshow(mfcc, ax=ax)
+            # fig_path = mfcc_root_path + uuid + '.png'
+            # fig.savefig(fig_path)
             wav_path = root_path + uuid + '.wav'
             y, sr = librosa.load(wav_path)
-            S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
-            log_S = librosa.power_to_db(S, ref=np.max)
-            mfcc = librosa.feature.mfcc(S=log_S, n_mfcc=13)
-            fig = plt.Figure()
-            canvas = FigureCanvas(fig)
-            ax = fig.add_subplot(111)
-            p = librosa.display.specshow(mfcc, ax=ax)
-            fig_path = mfcc_root_path + uuid + '.png'
-            fig.savefig(fig_path)
+            y = librosa.resample(y, sr, sr // 2)
+            sr = sr // 2
+            chunk_size = 2 * sr  # 2 seconds per chunk
+            chunks = librosa.util.frame(y, frame_length=chunk_size, hop_length=chunk_size)
+            for chunk in chunks.T:
+                total_count += 1
+                S = librosa.feature.melspectrogram(y=chunk, sr=sr, n_mels=128)
+                log_S = librosa.power_to_db(S, ref=np.max)
+                mfcc = librosa.feature.mfcc(S=log_S, n_mfcc=13)
+
+        print(total_count)
 
     elif sys.argv[1] == 'wav2spec':
-        spec_root_path = root_path + 'spec/'
+        spec_root_path = root_path + 'spec_chunk/'
+        total_count = 0
         for uuid in tqdm(list(uuid_set)):
             wav_path = root_path + uuid + '.wav'
             y, sr = librosa.load(wav_path)
-            fig = plt.Figure()
-            canvas = FigureCanvas(fig)
-            ax = fig.add_subplot(111)
-            S = librosa.feature.melspectrogram(y, sr=sr, n_mels=128)
-            log_S = librosa.power_to_db(S, ref=np.max)
-            librosa.display.specshow(log_S, sr=sr, ax=ax)
-            fig_path = spec_root_path + uuid + '.png'
-            fig.savefig(fig_path)
+            sr_new = 11025
+            y = librosa.resample(y, sr, sr_new)
+            sr = sr_new
+            chunk_size = 2 * sr  # 2 seconds per chunk
+            y_padded = librosa.util.fix_length(y, ((len(y) // chunk_size) + 1)* chunk_size)
+            chunks = librosa.util.frame(y_padded, frame_length=chunk_size, hop_length=chunk_size)
+            for i, chunk in enumerate(chunks.T):
+                total_count += 1
+                fig = plt.Figure()
+                canvas = FigureCanvas(fig)
+                ax = fig.add_subplot(111)
+                S = librosa.feature.melspectrogram(y=chunk, sr=sr, n_mels=128)
+                log_S = librosa.power_to_db(S, ref=np.max)
+                librosa.display.specshow(log_S, sr=sr, ax=ax)
+                fig_path = spec_root_path + uuid + '-' + str(i) + '.png'
+                fig.savefig(fig_path)
+        print(total_count)
     
